00620c724e7a2da591d763da327bf8459772829c
Amine-Itani <123438294+Amine-Itani@users.noreply.github.com>
2024-09-04 09:45:18 +0900

    Update Quiz-078.py

--- Quiz-078.py
+++ Quiz-078.py
@@ -1,1 +1,25 @@
+# Quiz 078
+
+## Task
+
+<sub>Fig. 1 shows the task at hand</sub>
+
+## Algorithm
+```.py
+def parity_maker(k,n,p):
+    par = []
+    m = k+n
+    for i in range (0, m+1):
+        if i & 2**(p-1):
+            par.append(i)
+    return par
+
+test1 = parity_maker(3,4,3)
+print(test1)
+```
+<sub>Fig. 2 shows the algorithm used to tackle the task</sub>
+
+## Results and Evidence
+
+<sub>Fig. 3 shows the results of the task and evidence of completion</sub>
 
